edc933fd261bbce95cbc9eb2335da0eea91320f2
w2013 <w2013@e-mirim.hs.kr>
2022-04-25 14:43:16 +0900

    urls bookmark:delete

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailView, BookmarkUpdateView
+from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailView, BookmarkUpdateView, \
+    BookmarkDeleteView
 
 app_name = 'bookmark'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('add/', BookmarkCreateView.as_view(), name='add'),    #bookmark:add
     path('detail/<int:pk>/',BookmarkDetailView.as_view(), name='detail'),    #bookmark:detail  특정값을 가져오기 위해 int:pk를 사용한다.
     path('edit/<int:pk>/', BookmarkUpdateView.as_view(), name='edit'),      #bookmark:edit
+    path('delete/<int:pk>/',BookmarkDeleteView.as_view(), name='delete')    #bookmark:delete
 ]
